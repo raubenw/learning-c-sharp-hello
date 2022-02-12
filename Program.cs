b2d9757c800e6b3b0c66d8285aa318e75da727c4
raubenw <raubenw@gmail.com>
2022-02-12 21:41:11 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+
+        /// <summary>
+        /// This is a C# Learning introducion
+        /// </summary>
+        /// <param name='args'>An array of String arguments from the command line</param>
+        /// <returns>
+        /// Does not return anything
+        /// </returns>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Why, hello there " + str);
+
+            string nullString = null;
+            Console.WriteLine(nullString ?? "Ek het null waarde");
+            nullString ??= "Ek is nul daarom kry ek 'n waarde";
+            Console.WriteLine(nullString);
+        }
+
+    }
+}
